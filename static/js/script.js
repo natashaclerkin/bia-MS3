7368eaace696851af029a7c6feffcd1815de03e9
natashaclerkin <natashaclerkin@gmail.com>
2021-04-30 02:49:48 +0000

    Create submit recipe template

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
      let sidenavs = document.querySelectorAll(".sidenav");
      let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
 });
+
+
+// MaterializeCSS Select initialization
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+});
