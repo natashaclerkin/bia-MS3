e69bf03745a7025bbe247c73907dcfc0ec366032
natashaclerkin <natashaclerkin@gmail.com>
2021-05-01 17:02:51 +0000

    Allow user to submit recipe to db & testing images urls

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
+# configuration
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -26,12 +28,13 @@ def index():
 
 
 # recipes
-@app.route("/get_recipes")
-def get_recipes():
+@app.route("/all_recipes")
+def all_recipes():
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
 
+# signup
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -57,6 +60,7 @@ def register():
     return render_template("register.html")
 
 
+# login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -86,6 +90,7 @@ def login():
     return render_template("login.html")
 
 
+# user profile
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
@@ -98,6 +103,7 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+# logout
 @app.route("/logout")
 def logout():
     # remove user from session cookie
@@ -106,13 +112,32 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
+# add recipe
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    # Adding recipe to db
+    if request.method == "POST":
+        recipe = {
+            "recipe_name": request.form.get("recipe_name"),
+            "category_name": request.form.get("category_name"),
+            "img_url": request.form.get("img_url"),
+            "serves": request.form.get("serves"),
+            "prep_time": request.form.get("prep_time"),
+            "cook_time": request.form.get("cook_time"),
+            "ingredients": request.form.get("ingredients"),
+            "method": request.form.get("method"),
+            "description": request.form.get("description"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe is successfully added")
+        return redirect(url_for("all_recipes"))
     # Find categories in db
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_recipe.html", categories=categories)
 
 
+# run application
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
