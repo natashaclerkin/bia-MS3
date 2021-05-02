6354317c1cfed2d609c7b88c631ff327cfa4cecf
natashaclerkin <natashaclerkin@gmail.com>
2021-05-02 00:26:25 +0000

    Wire up recipe edit button

--- app.py
+++ app.py
@@ -144,6 +144,15 @@ def add_recipe():
     return render_template("add_recipe.html", categories=categories)
 
 
+# Edit recipe
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, categories=categories)
+
+
 # run application
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
